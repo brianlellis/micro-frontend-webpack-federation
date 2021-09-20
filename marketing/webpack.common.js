3858b64247f073d5d9ca9f4929f9a5688b1c373b
Brian Ellis <brian@briansresume.com>
2021-09-20 19:40:47 -0400

    setting up webpack for marketing remote package

---
+++ marketing/webpack.common.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: path.resolve( __dirname , 'node_modules' ),
+        use:  {
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-react' , '@babel/preset-env' ],
+            plugins: [ '@babel/plugin-transform-runtime' ]
+          }
+        }
+      }
+    ]
+  }
+};
