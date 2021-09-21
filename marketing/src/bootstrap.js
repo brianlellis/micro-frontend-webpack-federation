5ef893a764235fdad4bc2dce04d020d212b9b674
Brian Ellis <brian@briansresume.com>
2021-09-20 20:17:24 -0400

    setting up basic routing of marketing remote app

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const ENV_ISOLATE = () => 'development' === process.env.NODE_ENV && 'development' === document.body.id;
 
 const mount = ele => {
   const ELE = ENV_ISOLATE ? document.getElementById( 'app' ) : ele;
-  ReactDOM.render( <h1>Marketing App</h1> , ELE );
+  ReactDOM.render( <App /> , ELE );
 };
 if ( ENV_ISOLATE ) mount();
 
